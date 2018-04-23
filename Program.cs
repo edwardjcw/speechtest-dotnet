2b7aa39069fcc182110df784afa9d334e24d040a
Martin Singh-Blom <vaftrudner@gmail.com>
2018-04-23 11:55:28 +0200

    Fixed string format error

--- Program.cs
+++ Program.cs
@@ -87,7 +87,7 @@ namespace SynthesizeVoice
 
             var responseSerializer = new DataContractJsonSerializer(typeof(SynthesizeResponse));
             client.DefaultRequestHeaders.Accept.Clear();
-            var url = String.Format("https://texttospeech.googleapis.com/v1beta1/text:synthesize?key={}", apiKey);
+            var url = String.Format("https://texttospeech.googleapis.com/v1beta1/text:synthesize?key={0}", apiKey);
             var postResponse = client
                 .PostAsync(
                     url,
